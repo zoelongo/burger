9a991105ce9bfdd857ed824ff50bd61a0b624c63
Zoe Longo <zoelongo@yahoo.com>
2019-11-13 14:24:33 -0700

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,37 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "burger_db"
+});
+
+//if the server contains the variable, it connects to the jawsDB
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+//if no variable, it falls back on an explicitly defined local database
+} else {
+
+connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
