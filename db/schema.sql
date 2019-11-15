098fe105c256e6c5d4273d5ddaba2f8be2802b81
Zoe Longo <zoelongo@yahoo.com>
2019-11-14 22:54:57 -0700

    starting functionality

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burger_db;
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	burger_name varchar(250) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
