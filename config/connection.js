a6d7945aaede0f2d0be15bc7b1a9f24e1a8f3d4a
Zoe Longo <zoelongo@yahoo.com>
2019-11-16 15:00:47 -0700

    adding gif for readme

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 8889,
   user: "root",
   password: "root",
   database: "burger_db"
@@ -20,8 +20,8 @@ connection = mysql.createConnection({
     host: "localhost",
     port: 8889,
     user: "root",
-    password: "oVerton321!",
-    database: "burgers_db"
+    password: "root",
+    database: "burger_db"
   });
 }
 // Make connection.
